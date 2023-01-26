415456ce461f8f5b89320ed7d7e141884636276a
rnlfzi <rfaidzilah@gmail.com>
2023-01-26 14:56:22 +0700

    first up

---
+++ src/states/shared/action.test.js
@@ -0,0 +1,86 @@
+/**
+ *  skenario test
+ *
+ * - asyncPopulateUserandtalks thunk
+ *  - should dispatch action correctly when data fetching success
+ *  - should dispatch action and call alert correctly when data fetching failed
+ */
+
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+import { receiveTalksActionCreator } from '../talks/action';
+import { receiveUsersActionCreator } from '../users/action';
+import { asyncPopulateUserAndTalks } from './action';
+
+const fakeTalkResponse = [
+  {
+    id: 'talk-1',
+    text: 'Talk test 1',
+    user: 'user-1',
+    replyTo: '',
+    likes: [],
+    createdAt: '2022-09-22T10:06:55.588Z',
+  },
+];
+
+const fakeUserResponse = [
+  {
+    id: 'user-1',
+    name: 'user test 1',
+    photo: 'https://generated-image-url.jpg',
+  },
+];
+
+const fakeErrorResponse = new Error('Ups, something went wrong');
+
+describe('asyncPopulateUsersAndTalks thunk', () => {
+  beforeEach(() => {
+    // backup ori implementaation
+    api._getAllUsers = api.getAllUsers;
+    api._getAllTalks = api.getAllTalks;
+  });
+
+  afterEach(() => {
+    // restore ori implementation
+    api.getAllUsers = api._getAllUsers;
+    api.getAllTalks = api._getAllTalks;
+
+    delete api._getAllUsers;
+    delete api._getAllTalks;
+  });
+
+  it('should dispatch action correctly when data fetching success', async () => {
+    // arrange
+    // stub implementation
+    api.getAllUsers = () => Promise.resolve(fakeUserResponse);
+    api.getAllTalks = () => Promise.resolve(fakeTalkResponse);
+
+    // mock dispatch
+    const dispatch = jest.fn();
+    // action
+    await asyncPopulateUserAndTalks()(dispatch);
+    // assert
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(receiveTalksActionCreator(fakeTalkResponse));
+    expect(dispatch).toHaveBeenCalledWith(receiveUsersActionCreator(fakeUserResponse));
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+  });
+
+  it('should dispatch action and call alert correctly when data fetching failed', async () => {
+    // arrange
+    // stub implementation
+    api.getAllUsers = () => Promise.reject(fakeErrorResponse);
+    api.getAllTalks = () => Promise.reject(fakeErrorResponse);
+    // mock dispatch
+    const dispatch = jest.fn();
+    // mock alert
+    window.alert = jest.fn();
+
+    // action
+    await asyncPopulateUserAndTalks()(dispatch);
+    // assert
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+    expect(window.alert).toHaveBeenCalledWith(fakeErrorResponse.message);
+  });
+});
